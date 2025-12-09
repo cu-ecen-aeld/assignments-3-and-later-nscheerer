f69d5c0f84d0d64854531fc596cf5bc5e72f60f7
Nathaniel <natscheerer@gmail.com>
2025-12-08 21:50:31 -0500

    implement assignment 3 TODOs

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -9,15 +9,17 @@
 */
 bool do_system(const char *cmd)
 {
-
-/*
- * TODO  add your code here
- *  Call the system() function with the command set in the cmd
- *   and return a boolean true if the system() call completed with success
- *   or false() if it returned a failure
-*/
-
-    return true;
+    int result = system(cmd);
+    
+    if (result == -1) {
+        return false;
+    }
+    
+    if (WIFEXITED(result) && WEXITSTATUS(result) == 0) {
+        return true;
+    }
+    
+    return false;
 }
 
 /**
@@ -49,19 +51,38 @@ bool do_exec(int count, ...)
     // and may be removed
     command[count] = command[count];
 
-/*
- * TODO:
- *   Execute a system command by calling fork, execv(),
- *   and wait instead of system (see LSP page 161).
- *   Use the command[0] as the full path to the command to execute
- *   (first argument to execv), and use the remaining arguments
- *   as second argument to the execv() command.
- *
-*/
 
+    pid_t pid = fork();
+    
+    if (pid == -1) 
+    {
+        perror("fork");
+        va_end(args);
+        return false;
+    }
+    
+    if (pid == 0) 
+    {
+        execv(command[0], command);
+        
+        perror("execv");
+        exit(EXIT_FAILURE);
+    }
+    
+    int status;
+
+    if (waitpid(pid, &status, 0) == -1) 
+    {
+        perror("waitpid");
+        va_end(args);
+        return false;
+    }
+    
     va_end(args);
-
-    return true;
+    
+    if (WIFEXITED(status) && WEXITSTATUS(status) == 0) return true;
+    
+    return false;
 }
 
 /**
@@ -84,16 +105,51 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
     // and may be removed
     command[count] = command[count];
 
-
-/*
- * TODO
- *   Call execv, but first using https://stackoverflow.com/a/13784315/1446624 as a refernce,
- *   redirect standard out to a file specified by outputfile.
- *   The rest of the behaviour is same as do_exec()
- *
-*/
-
+    pid_t pid = fork();
+    
+    if (pid == -1) 
+    {
+        perror("fork");
+        va_end(args);
+        return false;
+    }
+    
+    if (pid == 0) 
+    {
+        int fd = open(outputfile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+        
+        if (fd == -1) 
+        {
+            perror("open");
+            exit(EXIT_FAILURE);
+        }
+        
+        if (dup2(fd, STDOUT_FILENO) == -1) 
+        {
+            perror("dup2");
+            close(fd);
+            exit(EXIT_FAILURE);
+        }
+        
+        close(fd);
+        
+        execv(command[0], command);
+        
+        perror("execv");
+        exit(EXIT_FAILURE);
+    }
+    
+    int status;
+    if (waitpid(pid, &status, 0) == -1) 
+    {
+        perror("waitpid");
+        va_end(args);
+        return false;
+    }
+    
     va_end(args);
-
-    return true;
+    
+    if (WIFEXITED(status) && WEXITSTATUS(status) == 0) return true;
+    
+    return false;
 }
